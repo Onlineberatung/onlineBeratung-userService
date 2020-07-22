36ae2d6ff3aa673b5998ea83dd5062cffd15e474
Moritz Bittl <bittl@open4business.de>
2020-07-22 11:50:20 +0200

    test: test for maven package error

--- src/test/java/de/caritas/cob/UserService/api/facade/AssignSessionFacadeTest.java
+++ src/test/java/de/caritas/cob/UserService/api/facade/AssignSessionFacadeTest.java
@@ -123,7 +123,7 @@ public class AssignSessionFacadeTest {
   public void assignSession_Should_ReturnInternalServerErrorAndLogError_WhenUserDoesNotHaveRocketChatIdInDb() {
 
     HttpStatus result = assignSessionFacade.assignSession(SESSION_WITHOUT_CONSULTANT_NO_RC_USER_ID,
-        CONSULTANT, false);
+        CONSULTANT_NO_RC_USER_ID, false);
 
     assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, result);
     verify(logService, times(1)).logAssignSessionFacadeError(Mockito.anyString());
