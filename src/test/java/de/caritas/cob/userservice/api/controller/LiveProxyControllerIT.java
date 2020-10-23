882c19546e526e18a46c774be3d15f1d118cc5f5
mebo4b <eberl@open4business.de>
2020-10-23 17:01:07 +0200

    feat: add endpoint and controller for triggering live events

---
+++ src/test/java/de/caritas/cob/userservice/api/controller/LiveProxyControllerIT.java
@@ -0,0 +1,52 @@
+package de.caritas.cob.userservice.api.controller;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import de.caritas.cob.userservice.api.service.liveevents.LiveEventNotificationService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(LiveProxyController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@TestPropertySource(properties = "spring.profiles.active=testing")
+public class LiveProxyControllerIT {
+
+  private static final String LIVE_EVENT_PATH = "/liveproxy/send";
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private LiveEventNotificationService liveEventNotificationService;
+
+  @Test
+  public void sendLiveEvent_Should_returnBadRequest_When_rcGroupIdIsNotProvided() throws Exception {
+    this.mockMvc.perform(post(LIVE_EVENT_PATH))
+        .andExpect(status().isBadRequest());
+
+    verifyZeroInteractions(liveEventNotificationService);
+  }
+
+  @Test
+  public void sendLiveEvent_Should_returnStatusOkAndUseMock_When_rcGroupIdIsProvided()
+      throws Exception {
+    this.mockMvc.perform(post(LIVE_EVENT_PATH).param("rcGroupId", "id"))
+        .andExpect(status().isOk());
+
+    verify(liveEventNotificationService, times(1)).sendLiveDirectMessageEventToUsers(eq("id"));
+  }
+
+}
