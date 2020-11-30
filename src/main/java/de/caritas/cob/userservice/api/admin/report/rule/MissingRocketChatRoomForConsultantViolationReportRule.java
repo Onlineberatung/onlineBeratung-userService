9499025876adff0f764d8bbb6faa0394d11f259d
mebo4b <eberl@open4business.de>
2020-11-30 17:10:50 +0100

    feat: provide rule for missing rocket chat rooms

---
+++ src/main/java/de/caritas/cob/userservice/api/admin/report/rule/MissingRocketChatRoomForConsultantViolationReportRule.java
@@ -0,0 +1,95 @@
+package de.caritas.cob.userservice.api.admin.report.rule;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
+
+import de.caritas.cob.userservice.api.admin.report.builder.ViolationByConsultantBuilder;
+import de.caritas.cob.userservice.api.admin.report.model.ViolationReportRule;
+import de.caritas.cob.userservice.api.model.ViolationDTO;
+import de.caritas.cob.userservice.api.model.rocketchat.user.UserInfoResponseDTO;
+import de.caritas.cob.userservice.api.model.rocketchat.user.UserRoomDTO;
+import de.caritas.cob.userservice.api.repository.consultant.Consultant;
+import de.caritas.cob.userservice.api.repository.consultant.ConsultantRepository;
+import de.caritas.cob.userservice.api.repository.session.Session;
+import de.caritas.cob.userservice.api.repository.session.SessionRepository;
+import de.caritas.cob.userservice.api.repository.session.SessionStatus;
+import de.caritas.cob.userservice.api.service.RocketChatService;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Violation rule to find consultants without neccessary rocket chat room.
+ */
+@Component
+@RequiredArgsConstructor
+public class MissingRocketChatRoomForConsultantViolationReportRule implements ViolationReportRule {
+
+  private final @NonNull ConsultantRepository consultantRepository;
+  private final @NonNull SessionRepository sessionRepository;
+  private final @NonNull RocketChatService rocketChatService;
+
+  /**
+   * Generates all violations for {@link Consultant} without required rocket chat room assignment.
+   *
+   * @return the generated violations
+   */
+  @Override
+  public List<ViolationDTO> generateViolations() {
+    return StreamSupport.stream(this.consultantRepository.findAll().spliterator(), true)
+        .map(consultant -> this.sessionRepository
+            .findByConsultantAndStatus(consultant, SessionStatus.IN_PROGRESS))
+        .flatMap(Collection::parallelStream)
+        .map(this::fromMissingSession)
+        .filter(Objects::nonNull)
+        .collect(Collectors.toList());
+  }
+
+  private ViolationDTO fromMissingSession(Session session) {
+    UserInfoResponseDTO userInfoWithRooms = this.rocketChatService
+        .getUserInfo(session.getConsultant().getRocketChatId());
+    List<UserRoomDTO> rooms = userInfoWithRooms.getUser().getRooms();
+    List<String> rocketChatRoomsOfUser = rooms.stream()
+        .map(UserRoomDTO::getRoomId)
+        .collect(Collectors.toList());
+
+    String violationMessage = buildPossibleViolationMessage(session, rocketChatRoomsOfUser);
+    if (isNotBlank(violationMessage)) {
+      return ViolationByConsultantBuilder.getInstance(session.getConsultant())
+          .withReason(violationMessage)
+          .build();
+    }
+    return null;
+  }
+
+  private String buildPossibleViolationMessage(Session session,
+      List<String> rocketChatRoomsOfUser) {
+    String violationMessage = "";
+
+    if (isGroupMissing(session.getGroupId(), rocketChatRoomsOfUser)) {
+      violationMessage += "Missing room with id " + session.getGroupId() + " in rocket chat";
+    }
+    if (areBothRoomsMissing(session, rocketChatRoomsOfUser)) {
+      violationMessage += " and ";
+    }
+    if (isGroupMissing(session.getFeedbackGroupId(), rocketChatRoomsOfUser)) {
+      violationMessage += "Missing feedback room with id " + session.getFeedbackGroupId() + " in "
+          + "rocket chat";
+    }
+    return violationMessage;
+  }
+
+  private boolean isGroupMissing(String groupId, List<String> rocketChatRooms) {
+    return isNotBlank(groupId) && !rocketChatRooms.contains(groupId);
+  }
+
+  private boolean areBothRoomsMissing(Session session, List<String> rocketChatRoomsOfUser) {
+    return isGroupMissing(session.getGroupId(), rocketChatRoomsOfUser) && isGroupMissing(
+        session.getFeedbackGroupId(), rocketChatRoomsOfUser);
+  }
+
+}
---
+++ src/test/java/de/caritas/cob/userservice/api/admin/report/rule/MissingRocketChatRoomForConsultantViolationReportRuleTest.java
@@ -0,0 +1,150 @@
+package de.caritas.cob.userservice.api.admin.report.rule;
+
+import static de.caritas.cob.userservice.api.model.ViolationDTO.ViolationTypeEnum.CONSULTANT;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static java.util.Collections.singleton;
+import static java.util.Collections.singletonList;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import de.caritas.cob.userservice.api.model.ViolationDTO;
+import de.caritas.cob.userservice.api.model.rocketchat.user.UserInfoResponseDTO;
+import de.caritas.cob.userservice.api.model.rocketchat.user.UserRoomDTO;
+import de.caritas.cob.userservice.api.repository.consultant.Consultant;
+import de.caritas.cob.userservice.api.repository.consultant.ConsultantRepository;
+import de.caritas.cob.userservice.api.repository.session.Session;
+import de.caritas.cob.userservice.api.repository.session.SessionRepository;
+import de.caritas.cob.userservice.api.service.RocketChatService;
+import java.util.List;
+import org.jeasy.random.EasyRandom;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MissingRocketChatRoomForConsultantViolationReportRuleTest {
+
+  @InjectMocks
+  private MissingRocketChatRoomForConsultantViolationReportRule reportRule;
+
+  @Mock
+  private ConsultantRepository consultantRepository;
+
+  @Mock
+  private SessionRepository sessionRepository;
+
+  @Mock
+  private RocketChatService rocketChatService;
+
+  @Test
+  public void generateViolations_Should_returnEmptyList_When_noViolationExists() {
+    List<ViolationDTO> violations = this.reportRule.generateViolations();
+
+    assertThat(violations, hasSize(0));
+  }
+
+  @Test
+  public void generateViolations_Should_returnExpectedViolation_When_oneViolatedSessionExists() {
+    Consultant violatedConsultant = new EasyRandom().nextObject(Consultant.class);
+    violatedConsultant.setSessions(null);
+    Session violatedSession = new EasyRandom().nextObject(Session.class);
+    UserInfoResponseDTO userInfoResponseDTO =
+        new EasyRandom().nextObject(UserInfoResponseDTO.class);
+    userInfoResponseDTO.getUser().setRooms(emptyList());
+
+    when(this.consultantRepository.findAll()).thenReturn(singletonList(violatedConsultant));
+    when(this.sessionRepository.findByConsultantAndStatus(any(), any()))
+        .thenReturn(singletonList(violatedSession));
+    when(this.rocketChatService.getUserInfo(any())).thenReturn(userInfoResponseDTO);
+
+    List<ViolationDTO> violations = this.reportRule.generateViolations();
+
+    assertThat(violations, hasSize(1));
+    ViolationDTO resultViolation = violations.iterator().next();
+    assertThat(resultViolation.getIdentifier(), is(violatedSession.getConsultant().getId()));
+    assertThat(resultViolation.getViolationType(), is(CONSULTANT));
+    assertThat(resultViolation.getReason(),
+        is("Missing room with id " + violatedSession.getGroupId()
+            + " in rocket chat and Missing feedback room with id " + violatedSession
+            .getFeedbackGroupId() + " in rocket chat"));
+  }
+
+  @Test
+  public void generateViolations_Should_returnViolationMessageOfFeedbackRoom_When_oneViolatedFeedbackSessionExists() {
+    Consultant violatedConsultant = new EasyRandom().nextObject(Consultant.class);
+    Session violatedSession = new EasyRandom().nextObject(Session.class);
+    violatedConsultant.setSessions(singleton(violatedSession));
+    UserInfoResponseDTO userInfoResponseDTO =
+        new EasyRandom().nextObject(UserInfoResponseDTO.class);
+    userInfoResponseDTO.getUser()
+        .setRooms(singletonList(new UserRoomDTO(violatedSession.getGroupId())));
+
+    when(this.consultantRepository.findAll()).thenReturn(singletonList(violatedConsultant));
+    when(this.sessionRepository.findByConsultantAndStatus(any(), any()))
+        .thenReturn(singletonList(violatedSession));
+    when(this.rocketChatService.getUserInfo(any())).thenReturn(userInfoResponseDTO);
+
+    List<ViolationDTO> violations = this.reportRule.generateViolations();
+
+    assertThat(violations, hasSize(1));
+    ViolationDTO resultViolation = violations.iterator().next();
+    assertThat(resultViolation.getIdentifier(), is(violatedSession.getConsultant().getId()));
+    assertThat(resultViolation.getViolationType(), is(CONSULTANT));
+    assertThat(resultViolation.getReason(),
+        is("Missing feedback room with id " + violatedSession.getFeedbackGroupId()
+            + " in rocket chat"));
+  }
+
+  @Test
+  public void generateViolations_Should_returnViolationMessageOfRoom_When_oneViolatedStandardSessionExists() {
+    Consultant violatedConsultant = new EasyRandom().nextObject(Consultant.class);
+    Session violatedSession = new EasyRandom().nextObject(Session.class);
+    violatedConsultant.setSessions(singleton(violatedSession));
+    UserInfoResponseDTO userInfoResponseDTO =
+        new EasyRandom().nextObject(UserInfoResponseDTO.class);
+    userInfoResponseDTO.getUser()
+        .setRooms(singletonList(new UserRoomDTO(violatedSession.getFeedbackGroupId())));
+
+    when(this.consultantRepository.findAll()).thenReturn(singletonList(violatedConsultant));
+    when(this.sessionRepository.findByConsultantAndStatus(any(), any()))
+        .thenReturn(singletonList(violatedSession));
+    when(this.rocketChatService.getUserInfo(any())).thenReturn(userInfoResponseDTO);
+
+    List<ViolationDTO> violations = this.reportRule.generateViolations();
+
+    assertThat(violations, hasSize(1));
+    ViolationDTO resultViolation = violations.iterator().next();
+    assertThat(resultViolation.getIdentifier(), is(violatedSession.getConsultant().getId()));
+    assertThat(resultViolation.getViolationType(), is(CONSULTANT));
+    assertThat(resultViolation.getReason(),
+        is("Missing room with id " + violatedSession.getGroupId() + " in rocket chat"));
+  }
+
+  @Test
+  public void generateViolations_Should_returnNoViolation_When_allRoomsExst() {
+    Consultant violatedConsultant = new EasyRandom().nextObject(Consultant.class);
+    Session violatedSession = new EasyRandom().nextObject(Session.class);
+    violatedConsultant.setSessions(singleton(violatedSession));
+    UserInfoResponseDTO userInfoResponseDTO =
+        new EasyRandom().nextObject(UserInfoResponseDTO.class);
+    userInfoResponseDTO.getUser().setRooms(asList(
+        new UserRoomDTO(violatedSession.getGroupId()),
+            new UserRoomDTO(violatedSession.getFeedbackGroupId())));
+
+    when(this.consultantRepository.findAll()).thenReturn(singletonList(violatedConsultant));
+    when(this.sessionRepository.findByConsultantAndStatus(any(), any()))
+        .thenReturn(singletonList(violatedSession));
+    when(this.rocketChatService.getUserInfo(any())).thenReturn(userInfoResponseDTO);
+
+    List<ViolationDTO> violations = this.reportRule.generateViolations();
+
+    assertThat(violations, hasSize(0));
+  }
+
+}
