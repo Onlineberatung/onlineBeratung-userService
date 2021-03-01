3da47b800942be6f8b4a7d3a81c1c649c7bc32cf
Moritz Bittl <bittl@open4business.de>
2021-03-01 09:41:27 +0100

    feat: added message service api client generation

--- src/main/java/de/caritas/cob/userservice/config/apiclient/AgencyAdminServiceApiClientConfig.java
+++ src/main/java/de/caritas/cob/userservice/config/apiclient/AgencyAdminServiceApiClientConfig.java
@@ -8,17 +8,32 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * Configuration class for the AgencyAdminService API client.
+ */
 @Component
 public class AgencyAdminServiceApiClientConfig {
 
   @Value("${agency.admin.service.api.url}")
   private String agencyAdminServiceApiUrl;
 
+  /**
+   * AgencyAdminService controller bean.
+   *
+   * @param apiClient {@link ApiClient}
+   * @return the LiveService controller {@link AdminAgencyControllerApi}
+   */
   @Bean
   public AdminAgencyControllerApi adminAgencyControllerApi(ApiClient apiClient) {
     return new AdminAgencyControllerApi(apiClient);
   }
 
+  /**
+   * AgencyAdminService API client bean.
+   *
+   * @param restTemplate {@link RestTemplate}
+   * @return the AgencyAdminService {@link ApiClient}
+   */
   @Bean
   @Primary
   public ApiClient adminAgencyApiClient(RestTemplate restTemplate) {
---
+++ src/main/java/de/caritas/cob/userservice/config/apiclient/MessageServiceApiClientConfig.java
@@ -0,0 +1,42 @@
+package de.caritas.cob.userservice.config.apiclient;
+
+import de.caritas.cob.userservice.messageservice.generated.ApiClient;
+import de.caritas.cob.userservice.messageservice.generated.web.MessageControllerApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Configuration class for the MessageService API client.
+ */
+@Configuration
+public class MessageServiceApiClientConfig {
+
+  @Value("${message.service.api.url}")
+  private String messageServiceApiUrl;
+
+  /**
+   * MessageService controller bean.
+   *
+   * @param messageServiceApiClient {@link ApiClient}
+   * @return the MessageService controller {@link MessageControllerApi}
+   */
+  @Bean
+  public MessageControllerApi messageControllerApi(ApiClient messageServiceApiClient) {
+    return new MessageControllerApi(messageServiceApiClient);
+  }
+
+  /**
+   * MessageService API client bean.
+   *
+   * @param restTemplate {@link RestTemplate}
+   * @return the MessageService {@link ApiClient}
+   */
+  @Bean
+  @Primary
+  public ApiClient messageServiceApiClient(RestTemplate restTemplate) {
+    return new ApiClient(restTemplate).setBasePath(this.messageServiceApiUrl);
+  }
+}
