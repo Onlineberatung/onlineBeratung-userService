d87424c0d1fcb8fe28b8887b3272a743e4ef7f08
Markus Eberl <eberl@open4business.de>
2021-03-18 16:31:38 +0100

    feat: integrate configurable firebase push notification service

--- src/main/java/de/caritas/cob/userservice/api/service/PushMessageService.java
+++ src/main/java/de/caritas/cob/userservice/api/service/PushMessageService.java
@@ -9,37 +9,71 @@ import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
 import java.io.InputStream;
 import lombok.SneakyThrows;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.stereotype.Service;
 
+/**
+ * Push service to send new message notifications via firebase to mobile devices.
+ */
+@Service
 public class PushMessageService {
 
-  public PushMessageService() {
-    initializeFirebase();
+  @Value("${firebase.configuration.push-notifications.enabled}")
+  private boolean isEnabled;
+
+  @Value("${firebase.configuration.credentials.file.path}")
+  private String firebaseConfiguration;
+
+  @Value("${firebase.configuration.notification.message}")
+  private String pushNotificationMessage;
+
+  private FirebaseMessaging firebaseMessaging;
+
+  /**
+   * Initializes the basic firebase configuration.
+   */
+  @SneakyThrows
+  @EventListener(ApplicationReadyEvent.class)
+  public void initializeFirebase() {
+    if (this.isEnabled) {
+      InputStream inputStream = PushMessageService.class.getResourceAsStream(firebaseConfiguration);
+
+      FirebaseOptions options = FirebaseOptions.builder()
+          .setCredentials(GoogleCredentials.fromStream(inputStream))
+          .build();
+
+      FirebaseApp.initializeApp(options);
+      this.firebaseMessaging = FirebaseMessaging.getInstance();
+    } else {
+      LogService.logInfo("Firebase push notifications are disabled");
+    }
   }
 
-  public void pushNewMessageEvent(String registrationToken) throws FirebaseMessagingException {
+  /**
+   * Sends a push notification message to mobile device with given registration token.
+   *
+   * @param registrationToken the mobile device identifier
+   */
+  public void pushNewMessageEvent(String registrationToken) {
+    if (!this.isEnabled) {
+      LogService.logInfo(
+          "Message event will not be pushed because firebase push notifications are disabled");
+      return;
+    }
     Message message = Message.builder()
         .setNotification(Notification.builder()
-            .setTitle("Neue Nachricht")
-            .setBody("Sie haben eine neue Nachricht")
+            .setBody(pushNotificationMessage)
             .build())
-        .putData("text", "test")
         .setToken(registrationToken)
         .build();
 
-    FirebaseMessaging.getInstance().send(message);
-  }
-
-  @SneakyThrows
-  private void initializeFirebase() {
-    InputStream inputStream =
-        PushMessageService.class
-            .getResourceAsStream("/firebase/firebase-credentials.json");
-
-    FirebaseOptions options = FirebaseOptions.builder()
-        .setCredentials(GoogleCredentials.fromStream(inputStream))
-        .build();
-
-    FirebaseApp.initializeApp(options);
+    try {
+      this.firebaseMessaging.send(message);
+    } catch (FirebaseMessagingException e) {
+      LogService.logWarn(e);
+    }
   }
 
 }
--- src/test/java/de/caritas/cob/userservice/api/service/PushMessageServiceTest.java
+++ src/test/java/de/caritas/cob/userservice/api/service/PushMessageServiceTest.java
@@ -1,16 +1,90 @@
 package de.caritas.cob.userservice.api.service;
 
-import com.google.firebase.messaging.FirebaseMessagingException;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Mockito.when;
+import static org.powermock.reflect.Whitebox.setInternalState;
+import static org.springframework.test.util.ReflectionTestUtils.getField;
+import static org.springframework.test.util.ReflectionTestUtils.setField;
 
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
+
+@RunWith(MockitoJUnitRunner.class)
 public class PushMessageServiceTest {
 
-  private final PushMessageService pushMessageService = new PushMessageService();
+  @InjectMocks
+  private PushMessageService pushMessageService;
+
+  @Mock
+  private FirebaseMessaging firebaseMessaging;
+
+  @Mock
+  private Logger logger;
+
+  @Before
+  public void setup() {
+    setField(pushMessageService, "firebaseMessaging", firebaseMessaging);
+    setInternalState(LogService.class, "LOGGER", logger);
+  }
 
   @Test
-  public void pushMessage_Should_pushMessage() throws FirebaseMessagingException {
-    String registrationToken = "crMYe-q0TYu5vHz7YnnNTY:APA91bEVtVeHPVNQYsolvhE9XU40BrJVzT8nFgLy6SZ-RH90YpLYuwnz9HHQHrRvYvpB2hraW1PQNYzAdjFSSapgbgNQ58PmIp44zyZmE-hVgswle_BnysxXTI4WERZMBBK7OAA3XPoJ";
-    this.pushMessageService.pushNewMessageEvent(registrationToken);
+  public void initializeFirebase_Should_notInitialiteFirebaseMessaging_When_firebaseIsDisabled() {
+    setField(this.pushMessageService, "isEnabled", false);
+
+    assertDoesNotThrow(() -> this.pushMessageService.initializeFirebase());
+
+    Object firebaseMessaging = getField(pushMessageService, "firebaseMessaging");
+    verify(this.logger, times(1)).info("Firebase push notifications are disabled");
+  }
+
+  @Test(expected = Exception.class)
+  public void initializeFirebase_Should_throwException_When_configurationCanNotBeLoaded() {
+    setField(this.pushMessageService, "isEnabled", true);
+    this.pushMessageService.initializeFirebase();
+  }
+
+  @Test
+  public void pushMessage_Should_pushFirebaseMessage() throws FirebaseMessagingException {
+    setField(this.pushMessageService, "isEnabled", true);
+
+    this.pushMessageService.pushNewMessageEvent("registrationToken");
+
+    verify(this.firebaseMessaging, times(1)).send(any());
+    verifyNoMoreInteractions(logger);
+  }
+
+  @Test
+  public void pushMessage_Should_logWarning_When_sendFails() throws FirebaseMessagingException {
+    setField(this.pushMessageService, "isEnabled", true);
+    FirebaseMessagingException exception = mock(FirebaseMessagingException.class);
+    when(this.firebaseMessaging.send(any())).thenThrow(exception);
+
+    this.pushMessageService.pushNewMessageEvent("registrationToken");
+
+    verify(logger, times(1)).warn(anyString());
+  }
+
+  @Test
+  public void pushMessage_Should_notSendNotification_When_firebaseIsDisabled() throws FirebaseMessagingException {
+    setField(this.pushMessageService, "isEnabled", false);
+
+    this.pushMessageService.pushNewMessageEvent("registrationToken");
+
+    verify(logger, times(1)).info(anyString());
+    verifyNoMoreInteractions(this.firebaseMessaging);
   }
 
 }
