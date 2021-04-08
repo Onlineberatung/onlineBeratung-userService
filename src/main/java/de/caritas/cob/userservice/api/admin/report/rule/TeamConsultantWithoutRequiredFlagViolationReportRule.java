ad22e510c25f8a64bd776b24dad882d73abeaecc
Markus Eberl <eberl@open4business.de>
2021-04-08 08:34:36 +0200

    feat: write violation reports to file

--- src/main/java/de/caritas/cob/userservice/api/admin/report/rule/TeamConsultantWithoutRequiredFlagViolationReportRule.java
+++ src/main/java/de/caritas/cob/userservice/api/admin/report/rule/TeamConsultantWithoutRequiredFlagViolationReportRule.java
@@ -5,7 +5,7 @@ import static org.apache.commons.lang3.BooleanUtils.isTrue;
 import de.caritas.cob.userservice.agencyadminserivce.generated.web.model.AgencyAdminResponseDTO;
 import de.caritas.cob.userservice.api.admin.report.builder.ViolationByConsultantBuilder;
 import de.caritas.cob.userservice.api.admin.report.model.ViolationReportRule;
-import de.caritas.cob.userservice.api.admin.report.service.AgencyAdminService;
+import de.caritas.cob.userservice.api.admin.service.agency.AgencyAdminService;
 import de.caritas.cob.userservice.api.model.ViolationDTO;
 import de.caritas.cob.userservice.api.repository.consultant.Consultant;
 import de.caritas.cob.userservice.api.repository.consultantagency.ConsultantAgency;
