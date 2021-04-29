55a8febb8aed860221b8722707e4b5091c4bcbdc
Markus Eberl <eberl@open4business.de>
2021-04-29 16:31:16 +0200

    feat: add cors mapping for registration endpoint

--- src/main/java/de/caritas/cob/userservice/config/CustomWebMvcConfigurer.java
+++ src/main/java/de/caritas/cob/userservice/config/CustomWebMvcConfigurer.java
@@ -2,19 +2,31 @@ package de.caritas.cob.userservice.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Component
-public class CustomSwaggerUIPathWebMvcConfigurer implements WebMvcConfigurer {
+public class CustomWebMvcConfigurer implements WebMvcConfigurer {
 
   @Value("${springfox.docuPath}")
   private String docuPath;
+
+  @Value("${registration.cors.allowed.origins}")
+  private String[] allowedOrigins;
 
   @Override
   public void addResourceHandlers(ResourceHandlerRegistry registry) {
     registry.addResourceHandler(docuPath + "/**")
         .addResourceLocations("classpath:/META-INF/resources/");
+  }
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/users/askers/new")
+        .allowCredentials(true)
+        .allowedMethods("OPTIONS", "POST")
+        .allowedOrigins(allowedOrigins);
   }
 
 }
