a92768c024b312389c92c0b097808836496ca6d0
Moritz Bittl <bittl@open4business.de>
2021-04-30 14:42:18 +0200

    chore: reduced varchar length

--- src/main/resources/db/changelog/changeset/0012_add_type_to_session/add-type-to-session.sql
+++ src/main/resources/db/changelog/changeset/0012_add_type_to_session/add-type-to-session.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `userservice`.`session`
-ADD `registration_type` varchar(255) COLLATE 'utf8_unicode_ci' NOT NULL DEFAULT 'REGISTERED'
+ADD `registration_type` varchar(20) COLLATE 'utf8_unicode_ci' NOT NULL DEFAULT 'REGISTERED'
     AFTER `consulting_type`;
