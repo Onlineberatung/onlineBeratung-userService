8208335a5daf41957586b14eda4bc58647df1c1e
Markus Eberl <eberl@open4business.de>
2021-09-08 14:28:45 +0200

    feat: provide asynchronus removal of consultants

--- src/main/java/de/caritas/cob/userservice/api/facade/assignsession/AssignEnquiryFacade.java
+++ src/main/java/de/caritas/cob/userservice/api/facade/assignsession/AssignEnquiryFacade.java
@@ -3,6 +3,7 @@ package de.caritas.cob.userservice.api.facade.assignsession;
 import static de.caritas.cob.userservice.api.repository.session.SessionStatus.IN_PROGRESS;
 import static de.caritas.cob.userservice.api.repository.session.SessionStatus.NEW;
 import static java.util.Objects.nonNull;
+import static java.util.concurrent.CompletableFuture.supplyAsync;
 
 import de.caritas.cob.userservice.api.admin.service.rocketchat.RocketChatRemoveFromGroupOperationService;
 import de.caritas.cob.userservice.api.exception.httpresponses.InternalServerErrorException;
@@ -11,11 +12,12 @@ import de.caritas.cob.userservice.api.manager.consultingtype.ConsultingTypeManag
 import de.caritas.cob.userservice.api.model.rocketchat.group.GroupMemberDTO;
 import de.caritas.cob.userservice.api.repository.consultant.Consultant;
 import de.caritas.cob.userservice.api.repository.session.Session;
+import de.caritas.cob.userservice.api.service.LogService;
 import de.caritas.cob.userservice.api.service.helper.KeycloakAdminClientService;
-import de.caritas.cob.userservice.api.service.rocketchat.RocketChatRollbackService;
 import de.caritas.cob.userservice.api.service.session.SessionService;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Supplier;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -31,7 +33,6 @@ public class AssignEnquiryFacade {
   private final @NonNull SessionService sessionService;
   private final @NonNull RocketChatFacade rocketChatFacade;
   private final @NonNull KeycloakAdminClientService keycloakAdminClientService;
-  private final @NonNull RocketChatRollbackService rocketChatRollbackService;
   private final @NonNull SessionToConsultantVerifier sessionToConsultantVerifier;
   private final @NonNull ConsultingTypeManager consultingTypeManager;
   private final @NonNull UnauthorizedMembersProvider unauthorizedMembersProvider;
@@ -46,10 +47,7 @@ public class AssignEnquiryFacade {
    */
   public void assignRegisteredEnquiry(Session session, Consultant consultant) {
     assignEnquiry(session, consultant);
-    updateRocketChatRooms(session.getGroupId(), session, consultant);
-    if (session.hasFeedbackChat()) {
-      updateRocketChatRooms(session.getFeedbackGroupId(), session, consultant);
-    }
+    supplyAsync(updateRocketChatRooms(session, consultant));
   }
 
   /**
@@ -73,6 +71,16 @@ public class AssignEnquiryFacade {
     }
   }
 
+  private Supplier<Object> updateRocketChatRooms(Session session, Consultant consultant) {
+    return () -> {
+      updateRocketChatRooms(session.getGroupId(), session, consultant);
+      if (session.hasFeedbackChat()) {
+        updateRocketChatRooms(session.getFeedbackGroupId(), session, consultant);
+      }
+      return null;
+    };
+  }
+
   private void assignEnquiry(Session session, Consultant consultant) {
     var consultantSessionDTO = ConsultantSessionDTO.builder()
         .consultant(consultant)
@@ -85,9 +93,8 @@ public class AssignEnquiryFacade {
   }
 
   private void updateRocketChatRooms(String rcGroupId, Session session, Consultant consultant) {
-    List<GroupMemberDTO> memberList = null;
     try {
-      memberList = this.rocketChatFacade.retrieveRocketChatMembers(rcGroupId);
+      var memberList = this.rocketChatFacade.retrieveRocketChatMembers(rcGroupId);
       removeUnauthorizedMembers(rcGroupId, session, consultant, memberList);
       if (session.hasFeedbackChat()) {
         this.rocketChatFacade
@@ -96,35 +103,26 @@ public class AssignEnquiryFacade {
       this.rocketChatFacade.removeSystemMessagesFromRocketChatGroup(rcGroupId);
 
     } catch (Exception e) {
-      initiateRollback(session, memberList);
+      LogService.logRocketChatError(e);
       throw e;
     }
   }
 
-  private void initiateRollback(Session session, List<GroupMemberDTO> groupMembers) {
-    this.rocketChatRollbackService.rollbackRemoveUsersFromRocketChatGroup(session.getGroupId(),
-        groupMembers);
-    this.rocketChatRollbackService
-        .rollbackRemoveUsersFromRocketChatGroup(session.getFeedbackGroupId(), groupMembers);
-    rollbackSessionUpdate(session);
-  }
-
   private void removeUnauthorizedMembers(String rcGroupId, Session session, Consultant consultant,
       List<GroupMemberDTO> memberList) {
-    List<Consultant> consultantsToRemoveFromRocketChat =
-        unauthorizedMembersProvider.obtainConsultantsToRemove(rcGroupId, session, consultant, memberList);
+    var consultantsToRemoveFromRocketChat = unauthorizedMembersProvider
+        .obtainConsultantsToRemove(rcGroupId, session, consultant, memberList);
+
+    var rocketChatRemoveFromGroupOperationService = RocketChatRemoveFromGroupOperationService
+        .getInstance(this.rocketChatFacade, this.keycloakAdminClientService,
+            this.consultingTypeManager)
+        .onSessionConsultants(Map.of(session, consultantsToRemoveFromRocketChat));
 
     if (rcGroupId.equalsIgnoreCase(session.getGroupId())) {
-      RocketChatRemoveFromGroupOperationService
-          .getInstance(this.rocketChatFacade, this.keycloakAdminClientService, this.consultingTypeManager)
-          .onSessionConsultants(Map.of(session, consultantsToRemoveFromRocketChat))
-          .removeFromGroupOrRollbackOnFailure();
+      rocketChatRemoveFromGroupOperationService.removeFromGroup();
     }
     if (rcGroupId.equalsIgnoreCase(session.getFeedbackGroupId())) {
-      RocketChatRemoveFromGroupOperationService
-          .getInstance(this.rocketChatFacade, this.keycloakAdminClientService, this.consultingTypeManager)
-          .onSessionConsultants(Map.of(session, consultantsToRemoveFromRocketChat))
-          .removeFromFeedbackGroupOrRollbackOnFailure();
+      rocketChatRemoveFromGroupOperationService.removeFromFeedbackGroup();
     }
   }
 
--- src/test/java/de/caritas/cob/userservice/api/facade/assignsession/AssignEnquiryFacadeTest.java
+++ src/test/java/de/caritas/cob/userservice/api/facade/assignsession/AssignEnquiryFacadeTest.java
@@ -1,5 +1,6 @@
 package de.caritas.cob.userservice.api.facade.assignsession;
 
+import static de.caritas.cob.userservice.api.repository.session.SessionStatus.IN_PROGRESS;
 import static de.caritas.cob.userservice.api.repository.session.SessionStatus.NEW;
 import static de.caritas.cob.userservice.testHelper.TestConstants.ANONYMOUS_ENQUIRY_WITHOUT_CONSULTANT;
 import static de.caritas.cob.userservice.testHelper.TestConstants.CONSULTANT_WITH_AGENCY;
@@ -12,6 +13,8 @@ import static de.caritas.cob.userservice.testHelper.TestConstants.ROCKET_CHAT_SY
 import static de.caritas.cob.userservice.testHelper.TestConstants.SESSION_WITHOUT_CONSULTANT;
 import static de.caritas.cob.userservice.testHelper.TestConstants.U25_SESSION_WITHOUT_CONSULTANT;
 import static java.util.Arrays.asList;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
 import static org.hibernate.validator.internal.util.CollectionHelper.asSet;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -23,6 +26,7 @@ import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.powermock.reflect.Whitebox.setInternalState;
 
 import de.caritas.cob.userservice.api.exception.httpresponses.InternalServerErrorException;
 import de.caritas.cob.userservice.api.exception.rocketchat.RocketChatAddUserToGroupException;
@@ -37,16 +41,19 @@ import de.caritas.cob.userservice.api.repository.session.SessionStatus;
 import de.caritas.cob.userservice.api.service.ConsultantService;
 import de.caritas.cob.userservice.api.service.LogService;
 import de.caritas.cob.userservice.api.service.helper.KeycloakAdminClientService;
-import de.caritas.cob.userservice.api.service.rocketchat.RocketChatRollbackService;
 import de.caritas.cob.userservice.api.service.session.SessionService;
 import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.function.Consumer;
 import org.jeasy.random.EasyRandom;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.slf4j.Logger;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AssignEnquiryFacadeTest {
@@ -67,13 +74,16 @@ public class AssignEnquiryFacadeTest {
   @Mock
   ConsultantService consultantService;
   @Mock
-  RocketChatRollbackService rocketChatRollbackService;
-  @Mock
   SessionToConsultantVerifier sessionToConsultantVerifier;
   @Mock
-  LogService logService;
+  Logger logger;
   @Mock
   UnauthorizedMembersProvider unauthorizedMembersProvider;
+
+  @Before
+  public void setup() {
+    setInternalState(LogService.class, "LOGGER", logger);
+  }
 
   @Test
   public void assignEnquiry_Should_ReturnOKAndNotRemoveSystemUser() {
@@ -107,7 +117,10 @@ public class AssignEnquiryFacadeTest {
         CONSULTANT_WITH_AGENCY);
     verify(rocketChatFacade, times(0)).removeUserFromGroup(ROCKET_CHAT_SYSTEM_USER_ID,
         RC_GROUP_ID);
-    verify(rocketChatFacade, times(1)).removeSystemMessagesFromRocketChatGroup(anyString());
+    await()
+        .atMost(1, SECONDS)
+        .until(verification((a) -> verify(rocketChatFacade, times(1))
+            .removeSystemMessagesFromRocketChatGroup(anyString())));
   }
 
   @Test
@@ -130,11 +143,11 @@ public class AssignEnquiryFacadeTest {
 
     verifyConsultantAndSessionHaveBeenChecked(FEEDBACKSESSION_WITHOUT_CONSULTANT,
         CONSULTANT_WITH_AGENCY);
-    verify(logService, times(1)).logInternalServerError(anyString(), any());
+    verify(logger, times(1)).error(anyString(), anyString());
   }
 
-  @Test(expected = InternalServerErrorException.class)
-  public void assignEnquiry_Should_ReturnInternalServerErrorAndLogErrorAndDoARollback_WhenRCRemoveGroupMembersFails() {
+  @Test
+  public void assignEnquiry_Should_LogError_When_RCRemoveGroupMembersFails() {
     doThrow(new InternalServerErrorException("")).when(rocketChatFacade)
         .removeSystemMessagesFromRocketChatGroup(anyString());
 
@@ -146,8 +159,10 @@ public class AssignEnquiryFacadeTest {
     verify(sessionService, times(1)).updateConsultantAndStatusForSession(
         U25_SESSION_WITHOUT_CONSULTANT, CONSULTANT_WITH_AGENCY,
         SessionStatus.IN_PROGRESS);
-    verify(rocketChatRollbackService, times(1))
-        .rollbackRemoveUsersFromRocketChatGroup(anyString(), Mockito.any());
+    await()
+        .atMost(1, SECONDS)
+        .until(verification(
+            (a) -> verify(logger, times(1)).error(anyString(), anyString(), anyString())));
   }
 
   @Test(expected = InternalServerErrorException.class)
@@ -160,14 +175,14 @@ public class AssignEnquiryFacadeTest {
 
     verifyConsultantAndSessionHaveBeenChecked(FEEDBACKSESSION_WITHOUT_CONSULTANT,
         CONSULTANT_WITH_AGENCY);
-    verify(logService, atLeast(2)).logInternalServerError(anyString());
+    verify(logger, atLeast(2)).error(anyString(), anyString());
     verify(sessionService, times(1)).updateConsultantAndStatusForSession(
         FEEDBACKSESSION_WITHOUT_CONSULTANT, FEEDBACKSESSION_WITHOUT_CONSULTANT.getConsultant(),
         FEEDBACKSESSION_WITHOUT_CONSULTANT.getStatus());
   }
 
-  @Test(expected = InternalServerErrorException.class)
-  public void assignEnquiry_Should_ReturnInternalServerErrorAndLogErrorAndDoARollback_WhenAddPeerConsultantToFeedbackGroupFails() {
+  @Test
+  public void assignEnquiry_Should_ReturnInternalServerErrorAndLogError_WhenAddPeerConsultantToFeedbackGroupFails() {
     doThrow(new InternalServerErrorException("")).when(rocketChatFacade)
         .addUserToRocketChatGroup(ROCKETCHAT_ID,
             RC_FEEDBACK_GROUP_ID);
@@ -177,17 +192,16 @@ public class AssignEnquiryFacadeTest {
 
     verifyConsultantAndSessionHaveBeenChecked(U25_SESSION_WITHOUT_CONSULTANT,
         CONSULTANT_WITH_AGENCY);
-    verify(logService, times(1)).logInternalServerError(anyString(),
-        Mockito.eq(RC_ADD_USER_TO_GROUP_EXC));
+    await()
+        .atMost(1, SECONDS)
+        .until(verification(
+            (a) -> verify(logger, times(1)).error(anyString(), anyString(), anyString())));
     verify(sessionService, times(1)).updateConsultantAndStatusForSession(
-        U25_SESSION_WITHOUT_CONSULTANT, U25_SESSION_WITHOUT_CONSULTANT.getConsultant(),
-        U25_SESSION_WITHOUT_CONSULTANT.getStatus());
-    verify(rocketChatRollbackService, times(1))
-        .rollbackRemoveUsersFromRocketChatGroup(anyString(), Mockito.any());
+        U25_SESSION_WITHOUT_CONSULTANT, CONSULTANT_WITH_AGENCY, IN_PROGRESS);
   }
 
-  @Test(expected = InternalServerErrorException.class)
-  public void assignEnquiry_Should_ReturnInternalServerErrorAndLogErrorAndDoARollback_WhenRemoveSystemMessagesFromGroupFails() {
+  @Test
+  public void assignEnquiry_Should_LogError_WhenRemoveSystemMessagesFromGroupFails() {
     doThrow(new InternalServerErrorException("error")).when(rocketChatFacade)
         .removeSystemMessagesFromRocketChatGroup(Mockito.any());
 
@@ -196,16 +210,16 @@ public class AssignEnquiryFacadeTest {
 
     verifyConsultantAndSessionHaveBeenChecked(U25_SESSION_WITHOUT_CONSULTANT,
         CONSULTANT_WITH_AGENCY);
-    verify(logService, times(1)).logInternalServerError(anyString());
+    await()
+        .atMost(1, SECONDS)
+        .until(verification(
+            (a) -> verify(logger, times(1)).error(anyString(), anyString(), anyString())));
     verify(sessionService, times(1)).updateConsultantAndStatusForSession(
-        U25_SESSION_WITHOUT_CONSULTANT, U25_SESSION_WITHOUT_CONSULTANT.getConsultant(),
-        U25_SESSION_WITHOUT_CONSULTANT.getStatus());
-    verify(rocketChatRollbackService, times(1))
-        .rollbackRemoveUsersFromRocketChatGroup(anyString(), Mockito.any());
+        U25_SESSION_WITHOUT_CONSULTANT, CONSULTANT_WITH_AGENCY, IN_PROGRESS);
   }
 
-  @Test(expected = InternalServerErrorException.class)
-  public void assignEnquiry_Should_ReturnInternalServerErrorAndLogErrorAndDoARollback_WhenRemoveSystemMessagesFromFeedbackChatFails() {
+  @Test
+  public void assignEnquiry_Should_LogError_When_RemoveSystemMessagesFromFeedbackChatFails() {
     doThrow(new InternalServerErrorException("error")).when(rocketChatFacade)
         .removeSystemMessagesFromRocketChatGroup(Mockito.any());
 
@@ -214,12 +228,19 @@ public class AssignEnquiryFacadeTest {
 
     verifyConsultantAndSessionHaveBeenChecked(U25_SESSION_WITHOUT_CONSULTANT,
         CONSULTANT_WITH_AGENCY);
-    verify(logService, times(1)).logInternalServerError(anyString());
+    await()
+        .atMost(1, SECONDS)
+        .until(verification(
+            (a) -> verify(logger, times(1)).error(anyString(), anyString(), anyString())));
     verify(sessionService, times(1)).updateConsultantAndStatusForSession(
-        U25_SESSION_WITHOUT_CONSULTANT, U25_SESSION_WITHOUT_CONSULTANT.getConsultant(),
-        U25_SESSION_WITHOUT_CONSULTANT.getStatus());
-    verify(rocketChatRollbackService, times(1))
-        .rollbackRemoveUsersFromRocketChatGroup(anyString(), Mockito.any());
+        U25_SESSION_WITHOUT_CONSULTANT, CONSULTANT_WITH_AGENCY, IN_PROGRESS);
+  }
+
+  private Callable<Boolean> verification(Consumer<Void> verificationFunction) {
+    return () -> {
+      verificationFunction.accept(null);
+      return true;
+    };
   }
 
   @Test
@@ -229,8 +250,12 @@ public class AssignEnquiryFacadeTest {
 
     verifyConsultantAndSessionHaveBeenChecked(U25_SESSION_WITHOUT_CONSULTANT,
         CONSULTANT_WITH_AGENCY);
-    verify(rocketChatFacade, times(1)).addUserToRocketChatGroup(ROCKETCHAT_ID,
-        U25_SESSION_WITHOUT_CONSULTANT.getFeedbackGroupId());
+    await()
+        .atMost(1, SECONDS)
+        .until(verification(
+            (a) -> verify(rocketChatFacade, times(1))
+                .addUserToRocketChatGroup(ROCKETCHAT_ID,
+                    U25_SESSION_WITHOUT_CONSULTANT.getFeedbackGroupId())));
   }
 
   @Test
@@ -260,8 +285,10 @@ public class AssignEnquiryFacadeTest {
     this.assignEnquiryFacade.assignRegisteredEnquiry(session, consultant);
 
     verifyConsultantAndSessionHaveBeenChecked(session, consultant);
-    verify(this.rocketChatFacade, times(1))
-        .removeUserFromGroup(consultantToRemove.getRocketChatId(), session.getGroupId());
+    await()
+        .atMost(1, SECONDS)
+        .until(verification((a) -> verify(this.rocketChatFacade, times(1))
+            .removeUserFromGroup(consultantToRemove.getRocketChatId(), session.getGroupId())));
     verify(this.rocketChatFacade, times(1))
         .removeUserFromGroup(consultantToRemove.getRocketChatId(), session.getFeedbackGroupId());
   }
@@ -295,8 +322,10 @@ public class AssignEnquiryFacadeTest {
     this.assignEnquiryFacade.assignRegisteredEnquiry(session, consultant);
 
     verifyConsultantAndSessionHaveBeenChecked(session, consultant);
-    verify(this.rocketChatFacade, atLeastOnce())
-        .removeUserFromGroup(consultantToRemove.getRocketChatId(), session.getGroupId());
+    await()
+        .atMost(1, SECONDS)
+        .until(verification((a) -> verify(this.rocketChatFacade, atLeastOnce())
+            .removeUserFromGroup(consultantToRemove.getRocketChatId(), session.getGroupId())));
     verify(this.rocketChatFacade, atLeastOnce())
         .removeUserFromGroup(consultantToRemove.getRocketChatId(), session.getFeedbackGroupId());
     verify(this.rocketChatFacade, never())
