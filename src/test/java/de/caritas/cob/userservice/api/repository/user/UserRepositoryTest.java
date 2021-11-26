3800fe2af429841a034c50eb6506c400d0c2935c
Torsten Krohn <torsten.krohn@webatspeed.com>
2021-11-26 10:08:42 +0100

    feat: check user search by tomorrow date

--- src/test/java/de/caritas/cob/userservice/api/repository/user/UserRepositoryTest.java
+++ src/test/java/de/caritas/cob/userservice/api/repository/user/UserRepositoryTest.java
@@ -3,8 +3,11 @@ package de.caritas.cob.userservice.api.repository.user;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,16 +28,10 @@ public class UserRepositoryTest {
   private UserRepository userRepository;
 
   @Test
-  public void findAllByDeleteDateNullAndNoRunningSessionsAndCreateDateOlderThan_Should_ReturnCorrectUsers() {
-
+  public void findAllByDeleteDateNullAndNoRunningSessionsAndCreateDateOlderThanShouldReturnUserSubsetOnPastDate() {
     final LocalDateTime dateCheck = LocalDateTime.of(2019, 1, 1, 0, 0, 0, 0);
     final String USER_ID_SHOULD_IN_LIST_1 = "abc9a0a1-c936-45ee-9141-d73dfc0a3999";
     final String USER_ID_SHOULD_IN_LIST_2 = "def9a0a1-c936-45ee-9141-d73dfc0a3888";
-    final String USER_ID_SHOULD_NOT_IN_LIST_1 = "hki9a0a1-c936-45ee-9141-d73dfc0a3777";
-    final String USER_ID_SHOULD_NOT_IN_LIST_2 = "dlk9a0a1-c936-45ee-9141-d73dfc0a3666";
-    final String USER_ID_SHOULD_NOT_IN_LIST_3 = "arb9a0a1-c936-45ee-9141-d73dfc0a3111";
-    final String USER_ID_SHOULD_NOT_IN_LIST_4 = "jurea0a1-c936-45ee-9141-d73dfc0a3000";
-    final String USER_ID_SHOULD_NOT_IN_LIST_5 = "opiti0a1-c936-45ee-9141-d73dfc0a3000";
 
     List<User> result =
         userRepository.findAllByDeleteDateNullAndNoRunningSessionsAndCreateDateOlderThan(dateCheck);
@@ -45,16 +42,16 @@ public class UserRepositoryTest {
         is(true));
     assertThat(result.stream().map(User::getUserId).anyMatch(USER_ID_SHOULD_IN_LIST_2::equals),
         is(true));
-    assertThat(result.stream().map(User::getUserId).anyMatch(USER_ID_SHOULD_NOT_IN_LIST_1::equals),
-        is(false));
-    assertThat(result.stream().map(User::getUserId).anyMatch(USER_ID_SHOULD_NOT_IN_LIST_2::equals),
-        is(false));
-    assertThat(result.stream().map(User::getUserId).anyMatch(USER_ID_SHOULD_NOT_IN_LIST_3::equals),
-        is(false));
-    assertThat(result.stream().map(User::getUserId).anyMatch(USER_ID_SHOULD_NOT_IN_LIST_4::equals),
-        is(false));
-    assertThat(result.stream().map(User::getUserId).anyMatch(USER_ID_SHOULD_NOT_IN_LIST_5::equals),
-        is(false));
   }
 
+  @Test
+  public void findAllByDeleteDateNullAndNoRunningSessionsAndCreateDateOlderThanShouldReturnAllUsersOnTomorrowAsDate() {
+    var startOfTomorrow = LocalDateTime.now().with(LocalTime.MIDNIGHT).plusDays(100);
+
+    var users = userRepository.findAllByDeleteDateNullAndNoRunningSessionsAndCreateDateOlderThan(
+        startOfTomorrow);
+
+    assertNotNull(users);
+    assertEquals(users.size(), 12);
+  }
 }
