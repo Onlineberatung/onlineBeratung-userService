8bdac7082feef5324cec6235c15930ec239aed67
Markus Eberl <eberl@open4business.de>
2022-01-14 08:39:04 +0100

    feat: provide cronjob for regular notification mails of open enquiries

---
+++ src/main/java/de/caritas/cob/userservice/api/enquirynotification/scheduler/EnquiryNotificationScheduler.java
@@ -0,0 +1,34 @@
+package de.caritas.cob.userservice.api.enquirynotification.scheduler;
+
+import static org.apache.commons.lang3.BooleanUtils.isTrue;
+
+import de.caritas.cob.userservice.api.enquirynotification.service.EnquiryNotificationService;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * Scheduler email notifications of open enquiries.
+ */
+@Component
+@RequiredArgsConstructor
+public class EnquiryNotificationScheduler {
+
+  private final @NonNull EnquiryNotificationService enquiryNotificationService;
+
+  @Value("${enquiry.open.notification.enabled}")
+  private Boolean enquiryNotificationsEnabled;
+
+  /**
+   * Entry method to build and send email notifications.
+   */
+  @Scheduled(cron = "${enquiry.open.notification.cron}")
+  public void sendEmailNotificationsForOpenEnquiries() {
+    if (isTrue(enquiryNotificationsEnabled)) {
+      enquiryNotificationService.sendEmailNotificationsForOpenEnquiries();
+    }
+  }
+
+}
