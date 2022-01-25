c340322f8a54a6bc6f81e0467f62f73f23cf7561
aalicic <adnan.alicic@dccs.at>
2022-01-25 23:20:16 +0100

    VIC-35: Make all data accessible only per tenant VIC-177: Extend data model - userservice

--- docker-build.cmd
+++ docker-build.cmd
@@ -1,3 +1,1 @@
-docker rmi cob/userservice:development
 docker build --no-cache -t cob/userservice:development .
-minikube cache add cob/userservice:development
--- docker-build.sh
+++
@@ -1,6 +0,0 @@
-docker rmi cob/userservice:development11
-minikube cache delete cob/userservice:development11
-mvn clean package -Dmaven.test.skip
-docker build --no-cache -t cob/userservice:development12 .
-minikube cache add cob/userservice:development12
-minikube cache reload cob/userservice:development12
