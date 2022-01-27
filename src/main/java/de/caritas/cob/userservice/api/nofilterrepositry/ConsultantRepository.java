641510303cf259e78499835a3a3c836840ade1af
aalicic <adnan.alicic@dccs.at>
2022-01-27 15:45:02 +0100

    VIC-35: Make all data accessible only per tenant VIC-177: Extend data model - userservice

---
+++ src/main/java/de/caritas/cob/userservice/api/nofilterrepositry/ConsultantRepository.java
@@ -0,0 +1,23 @@
+package de.caritas.cob.userservice.api.nofilterrepositry;
+
+import de.caritas.cob.userservice.api.repository.consultant.Consultant;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ConsultantRepository extends CrudRepository<Consultant, Long> {
+
+  Optional<Consultant> findByIdAndDeleteDateIsNull(String id);
+
+  Optional<Consultant> findByRocketChatIdAndDeleteDateIsNull(String id);
+
+  Optional<Consultant> findByEmailAndDeleteDateIsNull(String email);
+
+  Optional<Consultant> findByUsernameAndDeleteDateIsNull(String username);
+
+  List<Consultant> findByConsultantAgenciesAgencyIdInAndDeleteDateIsNull(List<Long> agencyIds);
+
+  List<Consultant> findByConsultantAgenciesAgencyIdAndDeleteDateIsNull(Long agencyId);
+
+  List<Consultant> findAllByDeleteDateNotNull();
+}
