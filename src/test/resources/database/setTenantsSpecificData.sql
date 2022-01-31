f00904d43a1a3fbbd5fe866df48b21759d0b5706
aalicic <adnan.alicic@dccs.at>
2022-02-01 00:21:20 +0100

    VIC-35: Make all data accessible only per tenant add more integration tests

--- src/test/resources/database/setTenantsSpecificData.sql
+++ src/test/resources/database/setTenantsSpecificData.sql
@@ -1,2 +1,9 @@
-INSERT INTO CONSULTANT (`consultant_id`, `tenant_id`, `username`,`first_name`,`last_name`,`email`)
-VALUES ('0b3b1cc6-be98-4787-aa56-212259d811b8', '1', 'enc.MVWWSZ3SMF2GS33OFV2GKYRR', 'test', 'test', 'test');
+INSERT INTO CONSULTANT (`consultant_id`, `tenant_id`, `username`,`first_name`,`last_name`,`email`, `is_team_consultant`)
+VALUES ('0b3b1cc6-be98-4787-aa56-212259d811b8', '1', 'enc.MVWWSZ3SMF2GS33OFV2GKYRR', 'test1', 'test1', 'test1',1);
+INSERT INTO CONSULTANT (`consultant_id`, `tenant_id`, `username`,`first_name`,`last_name`,`email`, `is_team_consultant`)
+VALUES ('0b3b1cc6-be98-4787-aa56-212259d811b7', '1', 'enc.MVWWSZ3SMF2GS33OFV2GKYPP', 'test2', 'test2', 'test2',0);
+
+INSERT INTO CONSULTANT_AGENCY (`id`, `tenant_id`, `consultant_id`, `agency_id`)
+VALUES (100, '1', '0b3b1cc6-be98-4787-aa56-212259d811b8', 1);
+INSERT INTO CONSULTANT_AGENCY (`id`, `tenant_id`, `consultant_id`, `agency_id`)
+VALUES (101, '1', '0b3b1cc6-be98-4787-aa56-212259d811b7', 1);
