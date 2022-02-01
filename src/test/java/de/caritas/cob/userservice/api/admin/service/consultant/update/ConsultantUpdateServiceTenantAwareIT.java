57e6375227116eef5a5c5af06d8eeab5e1859183
aalicic <adnan@leads.ba>
2022-02-01 11:32:32 +0100

    VIC-35: Make all data accessible only per tenant added more integration tests

--- src/test/java/de/caritas/cob/userservice/api/admin/service/consultant/update/ConsultantUpdateServiceTenantAwareIT.java
+++ src/test/java/de/caritas/cob/userservice/api/admin/service/consultant/update/ConsultantUpdateServiceTenantAwareIT.java
@@ -1,21 +1,40 @@
 package de.caritas.cob.userservice.api.admin.service.consultant.update;
 
 import de.caritas.cob.userservice.UserServiceApplication;
+import de.caritas.cob.userservice.api.tenant.TenantContext;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = UserServiceApplication.class)
 @TestPropertySource(properties = "spring.profiles.active=testing")
 @AutoConfigureTestDatabase(replace = Replace.ANY)
-public class ConsultantUpdateServiceIT extends ConsultantUpdateServiceBase {
+@TestPropertySource(properties = "multitenancy.enabled=true")
+@Transactional
+@Sql(value = "/database/setTenantsSpecificData.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+public class ConsultantUpdateServiceTenantAwareIT extends ConsultantUpdateServiceBase {
 
-  protected String VALID_CONSULTANT_ID = "5674839f-d0a3-47e2-8f9c-bb49fc2ddbbe";
+  protected String VALID_CONSULTANT_ID = "0b3b1cc6-be98-4787-aa56-212259d811b8";
+
+  @Before
+  public void beforeTests() {
+    TenantContext.setCurrentTenant(1L);
+  }
+
+  @After
+  public void afterTests() {
+    TenantContext.clear();
+  }
 
   @Test
   public void updateConsultant_Should_returnUpdatedPersistedConsultant_When_inputDataIsValid() {
@@ -35,5 +54,6 @@ public class ConsultantUpdateServiceIT extends ConsultantUpdateServiceBase {
   protected String getValidConsultantId() {
     return VALID_CONSULTANT_ID;
   }
+
 
 }
