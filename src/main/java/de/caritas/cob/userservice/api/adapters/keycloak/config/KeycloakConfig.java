d5f946cb074e55efc6fc93965d1f2b40adbce890
Torsten Krohn <torsten.krohn@webatspeed.com>
2022-02-09 08:42:59 +0100

    chore: remove unused required args constructor

--- src/main/java/de/caritas/cob/userservice/api/adapters/keycloak/config/KeycloakConfig.java
+++ src/main/java/de/caritas/cob/userservice/api/adapters/keycloak/config/KeycloakConfig.java
@@ -1,12 +1,10 @@
 package de.caritas.cob.userservice.api.adapters.keycloak.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@RequiredArgsConstructor
 public class KeycloakConfig {
 
   @Bean
