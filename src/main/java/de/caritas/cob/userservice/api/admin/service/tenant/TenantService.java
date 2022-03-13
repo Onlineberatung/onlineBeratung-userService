1750be7a20a3eccc0391347f57fc9fcbcd82f680
aalicic <adnan@leads.ba>
2022-03-13 12:33:15 +0100

    added bean for TenantControllerApi

--- src/main/java/de/caritas/cob/userservice/api/admin/service/tenant/TenantService.java
+++ src/main/java/de/caritas/cob/userservice/api/admin/service/tenant/TenantService.java
@@ -3,24 +3,24 @@ package de.caritas.cob.userservice.api.admin.service.tenant;
 import de.caritas.cob.userservice.api.service.httpheader.SecurityHeaderSupplier;
 import de.caritas.cob.userservice.api.tenant.TenantContext;
 import de.caritas.cob.userservice.tenantservice.generated.ApiClient;
-import de.caritas.cob.userservice.tenantservice.generated.web.TenantControllerApi;
-import de.caritas.cob.userservice.tenantservice.generated.web.model.TenantDTO;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
+import de.caritas.cob.userservice.tenantservice.generated.web.TenantControllerApi;
+import de.caritas.cob.userservice.tenantservice.generated.web.model.TenantDTO;
 
 @Service
 @RequiredArgsConstructor
 public class TenantService {
 
-  private final @NonNull TenantControllerApi apiClient;
   private final @NonNull SecurityHeaderSupplier securityHeaderSupplier;
+  private final @NonNull TenantControllerApi tenantControllerApi;
 
   public TenantDTO getTenantById() throws RestClientException {
-    addDefaultHeaders(this.apiClient.getApiClient());
-    return this.apiClient.getTenantById(
+    addDefaultHeaders(this.tenantControllerApi.getApiClient());
+    return this.tenantControllerApi.getTenantById(
         TenantContext.getCurrentTenant());
   }
 
@@ -28,5 +28,6 @@ public class TenantService {
     HttpHeaders headers = this.securityHeaderSupplier.getKeycloakAndCsrfHttpHeaders();
     headers.forEach((key, value) -> apiClient.addDefaultHeader(key, value.iterator().next()));
   }
+
 
 }
--- src/main/java/de/caritas/cob/userservice/config/apiclient/TenantServiceApiClientConfig.java
+++ src/main/java/de/caritas/cob/userservice/config/apiclient/TenantServiceApiClientConfig.java
@@ -1,17 +1,23 @@
 package de.caritas.cob.userservice.config.apiclient;
 
 import de.caritas.cob.userservice.tenantservice.generated.ApiClient;
+import de.caritas.cob.userservice.tenantservice.generated.web.TenantControllerApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Configuration
 public class TenantServiceApiClientConfig {
 
   @Value("${tenant.service.api.url}")
   private String tenantServiceApiUrl;
+
+  @Bean
+  public TenantControllerApi tenantControllerApi(ApiClient apiClient) {
+    return new TenantControllerApi(apiClient);
+  }
 
   @Bean
   @Primary
