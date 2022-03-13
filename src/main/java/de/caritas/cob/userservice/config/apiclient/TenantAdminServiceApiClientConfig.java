772ab0a4a035f15a693833f03f934a1eb5112b21
aalicic <adnan@leads.ba>
2022-03-13 17:35:58 +0100

    defined config beans for admin context of tenants service

---
+++ src/main/java/de/caritas/cob/userservice/config/apiclient/TenantAdminServiceApiClientConfig.java
@@ -0,0 +1,30 @@
+package de.caritas.cob.userservice.config.apiclient;
+
+import de.caritas.cob.userservice.tenantadminservice.generated.ApiClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.client.RestTemplate;
+import de.caritas.cob.userservice.tenantadminservice.generated.web.TenantAdminControllerApi;
+
+@Configuration
+public class TenantAdminServiceApiClientConfig {
+
+  @Value("${tenant.service.api.url}")
+  private String tenantServiceApiUrl;
+
+  @Bean
+  public TenantAdminControllerApi tenantControllerApi(ApiClient apiClient) {
+    return new TenantAdminControllerApi(apiClient);
+  }
+
+  @Bean
+  @Primary
+  public ApiClient tenantApiClient(RestTemplate restTemplate) {
+    ApiClient apiClient = new TenantAdminServiceApiClient(
+        restTemplate);
+    apiClient.setBasePath(this.tenantServiceApiUrl);
+    return apiClient;
+  }
+}
