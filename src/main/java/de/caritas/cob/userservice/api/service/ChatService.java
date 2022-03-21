21fefb7b7dbcb5c373bf5e6d973ea310577a92f8
aalicic <adnan@leads.ba>
2022-03-21 13:50:35 +0100

    VIC-287: Update Services from OS: new features like direct link & languages!

--- src/main/java/de/caritas/cob/userservice/api/service/ChatService.java
+++ src/main/java/de/caritas/cob/userservice/api/service/ChatService.java
@@ -26,7 +26,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
-import javax.ws.rs.BadRequestException;
+import de.caritas.cob.userservice.api.exception.httpresponses.BadRequestException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
