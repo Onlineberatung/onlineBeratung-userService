bf7ac3c136ebba39819ebdb09ff8ad0624c17f66
Torsten Krohn <torsten.krohn@webatspeed.com>
2022-04-14 15:53:51 +0200

    feature: add query to find consultants by infix

--- src/main/java/de/caritas/cob/userservice/api/port/out/ConsultantRepository.java
+++ src/main/java/de/caritas/cob/userservice/api/port/out/ConsultantRepository.java
@@ -3,6 +3,8 @@ package de.caritas.cob.userservice.api.port.out;
 import de.caritas.cob.userservice.api.model.Consultant;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ConsultantRepository extends CrudRepository<Consultant, String> {
@@ -20,6 +22,18 @@ public interface ConsultantRepository extends CrudRepository<Consultant, String>
   List<Consultant> findByConsultantAgenciesAgencyIdAndDeleteDateIsNull(Long agencyId);
 
   List<Consultant> findAllByDeleteDateNotNull();
+
+  @Query(
+      value =
+          "FROM Consultant c "
+              + "WHERE c.deleteDate IS NULL"
+              + "  AND ("
+              + "   UPPER(c.firstName) LIKE CONCAT('%', UPPER(?1), '%')"
+              + "   OR UPPER(c.lastName) LIKE CONCAT('%', UPPER(?1), '%')"
+              + "   OR UPPER(c.email) LIKE CONCAT('%', UPPER(?1), '%')"
+              + "  )"
+  )
+  List<Consultant> findAllByInfix(String infix, Pageable pageable);
 
   long countByDeleteDateIsNull();
 
--- src/test/java/de/caritas/cob/userservice/api/port/out/ConsultantRepositoryIT.java
+++ src/test/java/de/caritas/cob/userservice/api/port/out/ConsultantRepositoryIT.java
@@ -10,10 +10,13 @@ import com.neovisionaries.i18n.LanguageCode;
 import de.caritas.cob.userservice.api.model.Appointment;
 import de.caritas.cob.userservice.api.model.Consultant;
 import de.caritas.cob.userservice.api.model.Language;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
-import lombok.NonNull;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.jeasy.random.EasyRandom;
 import org.junit.jupiter.api.AfterEach;
@@ -24,10 +27,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.TestPropertySource;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ActiveProfiles;
 
 @DataJpaTest
-@TestPropertySource(properties = "spring.profiles.active=testing")
+@ActiveProfiles("testing")
 @AutoConfigureTestDatabase(replace = Replace.ANY)
 public class ConsultantRepositoryIT {
 
@@ -35,6 +40,10 @@ public class ConsultantRepositoryIT {
 
   private Consultant consultant;
   private Consultant originalConsultant;
+
+  private List<String> matchingIds = new ArrayList<>();
+
+  private List<String> nonMatchingIds = new ArrayList<>();
 
   @Autowired
   private ConsultantRepository underTest;
@@ -52,6 +61,10 @@ public class ConsultantRepositoryIT {
     underTest.save(originalConsultant);
     consultant = null;
     originalConsultant = null;
+    matchingIds.forEach(id -> underTest.deleteById(id));
+    matchingIds = new ArrayList<>();
+    nonMatchingIds.forEach(id -> underTest.deleteById(id));
+    nonMatchingIds = new ArrayList<>();
   }
 
   @Test
@@ -180,6 +193,147 @@ public class ConsultantRepositoryIT {
     assertEquals(LanguageCode.de, languagesThen.iterator().next().getLanguageCode());
   }
 
+  @Test
+  public void findAllByInfixShouldFindConsultantWithMatchingInfixes() {
+    var infix = RandomStringUtils.randomAlphanumeric(4);
+    var firstNameMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsMatchingFirstName(firstNameMatching, infix);
+    var lastNameMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsMatchingLastName(lastNameMatching, infix);
+    var emailMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsMatchingEmail(emailMatching, infix);
+    var notMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsNotMatching(notMatching, infix);
+
+    var foundConsultants = underTest.findAllByInfix(infix, Pageable.unpaged());
+
+    int allMatching = firstNameMatching + lastNameMatching + emailMatching;
+    assertEquals(allMatching, foundConsultants.size());
+    assertEquals(allMatching, matchingIds.size());
+    foundConsultants.forEach(consultant ->
+        assertTrue(matchingIds.contains(consultant.getId()))
+    );
+  }
+
+  @Test
+  public void findAllByInfixShouldReturnEmptyResultIfNoneMatching() {
+    var infix = RandomStringUtils.randomAlphanumeric(4);
+    var notMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsNotMatching(notMatching, infix);
+
+    var foundConsultants = underTest.findAllByInfix(infix, Pageable.unpaged());
+
+    assertEquals(0, foundConsultants.size());
+    assertEquals(0, matchingIds.size());
+  }
+
+  @Test
+  public void findAllByInfixShouldBeLimitedIfLimitGiven() {
+    var infix = RandomStringUtils.randomAlphanumeric(16);
+    var limit = easyRandom.nextInt(100);
+    givenConsultantsMatchingEmail(limit + 1, infix);
+
+    var pageRequest = PageRequest.of(0, limit);
+    var foundConsultants = underTest.findAllByInfix(infix, pageRequest);
+
+    assertEquals(limit, foundConsultants.size());
+    assertEquals(limit + 1, matchingIds.size());
+    foundConsultants.forEach(consultant ->
+        assertTrue(matchingIds.contains(consultant.getId()))
+    );
+  }
+
+  @Test
+  public void findAllByInfixShouldSearchCaseInsensitive() {
+    var infix = RandomStringUtils.randomAlphanumeric(4);
+    var transformedInfix = easyRandom.nextBoolean() ? infix.toLowerCase() : infix.toUpperCase();
+    var firstNameMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsMatchingFirstName(firstNameMatching, transformedInfix);
+    var lastNameMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsMatchingLastName(lastNameMatching, transformedInfix);
+    var emailMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsMatchingEmail(emailMatching, transformedInfix);
+    var notMatching = easyRandom.nextInt(20) + 5;
+    givenConsultantsNotMatching(notMatching, transformedInfix);
+
+    var foundConsultants = underTest.findAllByInfix(infix, Pageable.unpaged());
+
+    int allMatching = firstNameMatching + lastNameMatching + emailMatching;
+    assertEquals(allMatching, foundConsultants.size());
+    assertEquals(allMatching, matchingIds.size());
+    foundConsultants.forEach(consultant ->
+        assertTrue(matchingIds.contains(consultant.getId()))
+    );
+  }
+
+  private void givenConsultantsMatchingFirstName(@PositiveOrZero int count, @NotNull String infix) {
+    while (count-- > 0) {
+      var dbConsultant = underTest.findAll().iterator().next();
+      var consultant = new Consultant();
+      BeanUtils.copyProperties(dbConsultant, consultant);
+      consultant.setId(UUID.randomUUID().toString());
+      consultant.setUsername(RandomStringUtils.randomAlphabetic(8));
+      consultant.setRocketChatId(RandomStringUtils.randomAlphabetic(8));
+      consultant.setFirstName(aStringWithInfix(infix));
+      consultant.setLastName(aStringWithoutInfix(infix));
+      consultant.setEmail(aValidEmailWithoutInfix(infix));
+
+      underTest.save(consultant);
+      matchingIds.add(consultant.getId());
+    }
+  }
+
+  private void givenConsultantsMatchingLastName(@PositiveOrZero int count, @NotNull String infix) {
+    while (count-- > 0) {
+      var dbConsultant = underTest.findAll().iterator().next();
+      var consultant = new Consultant();
+      BeanUtils.copyProperties(dbConsultant, consultant);
+      consultant.setId(UUID.randomUUID().toString());
+      consultant.setUsername(RandomStringUtils.randomAlphabetic(8));
+      consultant.setRocketChatId(RandomStringUtils.randomAlphabetic(8));
+      consultant.setFirstName(aStringWithoutInfix(infix));
+      consultant.setLastName(aStringWithInfix(infix));
+      consultant.setEmail(aValidEmailWithoutInfix(infix));
+
+      underTest.save(consultant);
+      matchingIds.add(consultant.getId());
+    }
+  }
+
+  private void givenConsultantsMatchingEmail(@PositiveOrZero int count, @NotNull String infix) {
+    while (count-- > 0) {
+      var dbConsultant = underTest.findAll().iterator().next();
+      var consultant = new Consultant();
+      BeanUtils.copyProperties(dbConsultant, consultant);
+      consultant.setId(UUID.randomUUID().toString());
+      consultant.setUsername(RandomStringUtils.randomAlphabetic(8));
+      consultant.setRocketChatId(RandomStringUtils.randomAlphabetic(8));
+      consultant.setFirstName(aStringWithoutInfix(infix));
+      consultant.setLastName(aStringWithoutInfix(infix));
+      consultant.setEmail(aValidEmailWithInfix(infix));
+
+      underTest.save(consultant);
+      matchingIds.add(consultant.getId());
+    }
+  }
+
+  private void givenConsultantsNotMatching(@PositiveOrZero int count, @NotNull String infix) {
+    while (count-- > 0) {
+      var dbConsultant = underTest.findAll().iterator().next();
+      var consultant = new Consultant();
+      BeanUtils.copyProperties(dbConsultant, consultant);
+      consultant.setId(UUID.randomUUID().toString());
+      consultant.setUsername(RandomStringUtils.randomAlphabetic(8));
+      consultant.setRocketChatId(RandomStringUtils.randomAlphabetic(8));
+      consultant.setFirstName(aStringWithoutInfix(infix));
+      consultant.setLastName(aStringWithoutInfix(infix));
+      consultant.setEmail(aValidEmailWithoutInfix(infix));
+
+      underTest.save(consultant);
+      nonMatchingIds.add(consultant.getId());
+    }
+  }
+
   private void givenACreatedConsultantWithAnAppointment() {
     var dbConsultant = underTest.findAll().iterator().next();
     consultant = new Consultant();
@@ -204,13 +358,46 @@ public class ConsultantRepositoryIT {
     consultant = underTest.findById(consultant.getId()).orElseThrow();
   }
 
-  @NonNull
   private String aValidEmail() {
     return RandomStringUtils.randomAlphabetic(8)
         + "@"
         + RandomStringUtils.randomAlphabetic(8)
         + "."
         + (easyRandom.nextBoolean() ? "de" : "com");
+  }
+
+  private String aValidEmailWithoutInfix(String infix) {
+    var email = infix;
+    while (email.contains(infix)) {
+      email = aValidEmail();
+    }
+
+    return email;
+  }
+
+  private String aValidEmailWithInfix(String infix) {
+    return RandomStringUtils.randomAlphabetic(3)
+        + infix
+        + RandomStringUtils.randomAlphabetic(3)
+        + "@"
+        + RandomStringUtils.randomAlphabetic(8)
+        + "."
+        + (easyRandom.nextBoolean() ? "de" : "com");
+  }
+
+  private String aStringWithoutInfix(String infix) {
+    var str = infix;
+    while (str.contains(infix)) {
+      str = RandomStringUtils.randomAlphanumeric(8);
+    }
+
+    return str;
+  }
+
+  private String aStringWithInfix(String infix) {
+    return RandomStringUtils.randomAlphabetic(4)
+        + infix
+        + RandomStringUtils.randomAlphabetic(4);
   }
 
   private void givenAnExistingConsultantSpeaking(LanguageCode... languageCodes) {
