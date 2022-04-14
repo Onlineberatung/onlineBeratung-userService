0d99aa66a3fb6d0a5aacc945503596fb09f49100
aalicic <adnan@leads.ba>
2022-04-14 13:25:50 +0200

    fixed sql script in from migration

--- src/main/resources/db/changelog/changeset/0020_consultant_status/consultant-status.sql
+++ src/main/resources/db/changelog/changeset/0020_consultant_status/consultant-status.sql
@@ -1,3 +1,3 @@
 ALTER TABLE `userservice`.`consultant`
 ADD COLUMN `status` varchar(21) NULL DEFAULT NULL AFTER `encourage_2fa`;
-UPDATE `userservice`.`consultant` SET `status` = `CREATED`;
+UPDATE `userservice`.`consultant` SET `status` = 'CREATED';
