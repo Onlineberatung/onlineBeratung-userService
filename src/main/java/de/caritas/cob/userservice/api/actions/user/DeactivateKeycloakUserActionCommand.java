fd5f4778fdf21eac81b98c45943f9cae069c25a0
Markus Eberl <eberl@open4business.de>
2022-04-25 12:53:57 +0200

    fix: integrate logging to analyze anonymous user deletion in more detail

--- src/main/java/de/caritas/cob/userservice/api/actions/user/DeactivateKeycloakUserActionCommand.java
+++ src/main/java/de/caritas/cob/userservice/api/actions/user/DeactivateKeycloakUserActionCommand.java
@@ -1,15 +1,19 @@
 package de.caritas.cob.userservice.api.actions.user;
 
+import static org.apache.commons.lang3.exception.ExceptionUtils.getStackTrace;
+
 import de.caritas.cob.userservice.api.actions.ActionCommand;
 import de.caritas.cob.userservice.api.model.User;
 import de.caritas.cob.userservice.api.port.out.IdentityClient;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 /**
  * Deactivates a user in Keycloak.
  */
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class DeactivateKeycloakUserActionCommand implements ActionCommand<User> {
@@ -23,7 +27,12 @@ public class DeactivateKeycloakUserActionCommand implements ActionCommand<User> 
    */
   @Override
   public void execute(User user) {
-    this.identityClient.deactivateUser(user.getUserId());
+    try {
+      this.identityClient.deactivateUser(user.getUserId());
+    } catch (Exception e) {
+      log.error("Unable to deactivate User with id {}", user.getUserId());
+      log.error(getStackTrace(e));
+    }
   }
 
 }
