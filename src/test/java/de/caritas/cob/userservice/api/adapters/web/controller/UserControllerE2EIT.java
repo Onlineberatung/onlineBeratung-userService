407ed6fa60b3912a931a8184fba68ec8f63d124c
Patric Dosch <patric.dosch@virtual-identity.com>
2022-08-04 17:31:40 +0200

    fix: VIC-1122 Digi Registration | Add ControllerIT to test that new attributes are saved in the session
    - add integration tests for the various data in registration (demographics, mainTopic, topics, type)
    - add a fixme for really assert, currently we only check, that the response is "created", but at that state of code, this is the only thing we can test

--- src/test/java/de/caritas/cob/userservice/api/adapters/web/controller/UserControllerE2EIT.java
+++ src/test/java/de/caritas/cob/userservice/api/adapters/web/controller/UserControllerE2EIT.java
@@ -1103,6 +1103,7 @@ class UserControllerE2EIT {
     )));
   }
 
+  // FIXME: does not test the "saved monitoring", see next fixme
   @Test
   void registerUserWithoutConsultingIdShouldSaveMonitoring() throws Exception {
     givenConsultingTypeServiceResponse();
@@ -1110,14 +1111,15 @@ class UserControllerE2EIT {
     givenAUserDTO();
 
     mockMvc.perform(
-        post("/users/askers/new")
-            .cookie(CSRF_COOKIE)
-            .header(CSRF_HEADER, CSRF_VALUE)
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(objectMapper.writeValueAsString(userDTO)))
+            post("/users/askers/new")
+                .cookie(CSRF_COOKIE)
+                .header(CSRF_HEADER, CSRF_VALUE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDTO)))
         .andExpect(status().isCreated());
   }
 
+  // FIXME: (for all registerUser tests) Currently, we cannot easily get the generated data. The API does not return anything from the generated user, which would lead us to write code for tests. Which I don't like. Idea, API could return the user or at least the ID, then for the user we could fetch the sessions (de.caritas.cob.userservice.api.port.out.SessionRepository#findByUser), since there is only one at the time, I think this would be an acceptable solution.
   @Test
   void registerUserWithoutConsultingIdShouldSaveCreateUserWithDemographicsData() throws Exception {
     ReflectionTestUtils
@@ -1136,6 +1138,52 @@ class UserControllerE2EIT {
 
     ReflectionTestUtils
         .setField(userVerifier, "demographicsFeatureEnabled", false);
+  }
+
+  @Test
+  void registerUserWithoutConsultingIdShouldSaveCreateUserWithCounsellingRelationData()
+      throws Exception {
+    givenConsultingTypeServiceResponse(2);
+    givenARealmResource();
+    givenAUserDTOWithCounsellingRelation();
+
+    mockMvc.perform(
+            post("/users/askers/new")
+                .cookie(CSRF_COOKIE)
+                .header(CSRF_HEADER, CSRF_VALUE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDTO)))
+        .andExpect(status().isCreated());
+  }
+
+  @Test
+  void registerUserWithoutConsultingIdShouldSaveCreateUserWithMainTopicData() throws Exception {
+    givenConsultingTypeServiceResponse(2);
+    givenARealmResource();
+    givenAUserDTOWithMainTopic();
+
+    mockMvc.perform(
+            post("/users/askers/new")
+                .cookie(CSRF_COOKIE)
+                .header(CSRF_HEADER, CSRF_VALUE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDTO)))
+        .andExpect(status().isCreated());
+  }
+
+  @Test
+  void registerUserWithoutConsultingIdShouldSaveCreateUserWithTopicsData() throws Exception {
+    givenConsultingTypeServiceResponse(2);
+    givenARealmResource();
+    givenAUserDTOWithTopics();
+
+    mockMvc.perform(
+            post("/users/askers/new")
+                .cookie(CSRF_COOKIE)
+                .header(CSRF_HEADER, CSRF_VALUE)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(userDTO)))
+        .andExpect(status().isCreated());
   }
 
   @Test
@@ -1197,6 +1245,21 @@ class UserControllerE2EIT {
   private void givenAUserDTOWithDemographics() {
     givenAUserDTO();
     userDTO.setUserGender("MALE");
+  }
+
+  private void givenAUserDTOWithCounsellingRelation() {
+    givenAUserDTO();
+    userDTO.setCounsellingRelation("RELATIVE_COUNSELLING");
+  }
+
+  private void givenAUserDTOWithMainTopic() {
+    givenAUserDTO();
+    userDTO.setMainTopicId(0);
+  }
+
+  private void givenAUserDTOWithTopics() {
+    givenAUserDTO();
+    userDTO.setTopicIds(List.of(0, 1));
   }
 
   private long aPositiveLong() {
