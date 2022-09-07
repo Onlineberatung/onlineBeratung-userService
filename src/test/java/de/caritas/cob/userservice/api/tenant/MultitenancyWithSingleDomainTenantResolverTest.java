ba02518dcb2fe43039f6c063844741b6ad3dffd9
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-09-07 14:36:01 +0200

    fix: tenant resolution in case of direct consultant link

--- src/test/java/de/caritas/cob/userservice/api/tenant/MultitenancyWithSingleDomainTenantResolverTest.java
+++ src/test/java/de/caritas/cob/userservice/api/tenant/MultitenancyWithSingleDomainTenantResolverTest.java
@@ -6,19 +6,29 @@ import static org.mockito.Mockito.when;
 
 import de.caritas.cob.userservice.api.adapters.web.dto.AgencyDTO;
 import de.caritas.cob.userservice.api.exception.httpresponses.BadRequestException;
+import de.caritas.cob.userservice.api.model.Consultant;
+import de.caritas.cob.userservice.api.service.ConsultantService;
 import de.caritas.cob.userservice.api.service.agency.AgencyService;
 import de.caritas.cob.userservice.api.service.httpheader.HttpHeadersResolver;
 import java.util.Optional;
 import javax.servlet.http.HttpServletRequest;
+import org.jeasy.random.EasyRandom;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.util.ReflectionTestUtils;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @ExtendWith(MockitoExtension.class)
 class MultitenancyWithSingleDomainTenantResolverTest {
+
+  public static final String CONSULTANT_ID = "cid-1234";
+  public static final long TENANT = 1L;
+  public static final long ANOTHER_TENANT = 2L;
 
   @InjectMocks
   MultitenancyWithSingleDomainTenantResolver multitenancyWithSingleDomainTenantResolver;
@@ -28,6 +38,15 @@ class MultitenancyWithSingleDomainTenantResolverTest {
   @Mock AgencyService agencyService;
 
   @Mock HttpHeadersResolver headersResolver;
+
+  @Mock ConsultantService consultantService;
+
+  @Mock private ServletRequestAttributes requestAttributes;
+
+  @AfterEach
+  public void tearDown() {
+    resetRequestAttributes();
+  }
 
   @Test
   void resolve_Should_ResolveToEmpty_When_FeatureMultitenancyWithSingleDomainIsDisabled() {
@@ -52,6 +71,27 @@ class MultitenancyWithSingleDomainTenantResolverTest {
 
   @Test
   void
+      resolve_Should_GetTenantIdFromConsultant_When_FeatureMultitenancyWithSingleDomainIsEnabledAndNoAgencyIdIsProvided() {
+    // given
+    givenRequestContextIsSet();
+    ReflectionTestUtils.setField(
+        multitenancyWithSingleDomainTenantResolver, "multitenancyWithSingleDomain", true);
+
+    when(headersResolver.findHeaderValue("agencyId")).thenReturn(Optional.empty());
+    when(request.getParameter("cid")).thenReturn(CONSULTANT_ID);
+
+    EasyRandom random = new EasyRandom();
+    Consultant consultant = random.nextObject(Consultant.class);
+    consultant.setTenantId(ANOTHER_TENANT);
+    when(consultantService.getConsultant(CONSULTANT_ID)).thenReturn(Optional.of(consultant));
+    // when
+    assertThat(multitenancyWithSingleDomainTenantResolver.canResolve(request)).isTrue();
+    assertThat(multitenancyWithSingleDomainTenantResolver.resolve(request))
+        .isEqualTo(Optional.of(ANOTHER_TENANT));
+  }
+
+  @Test
+  void
       resolve_Should_ThrowBadRequestException_When_AgencyIdProvidedInHeader_ButAgencyDoesNotContainValidTenantId() {
     // given
     ReflectionTestUtils.setField(
@@ -62,5 +102,14 @@ class MultitenancyWithSingleDomainTenantResolverTest {
     assertThrows(
         BadRequestException.class,
         () -> multitenancyWithSingleDomainTenantResolver.resolve(request));
+  }
+
+  private void resetRequestAttributes() {
+    RequestContextHolder.setRequestAttributes(null);
+  }
+
+  private void givenRequestContextIsSet() {
+    when(requestAttributes.getRequest()).thenReturn(request);
+    RequestContextHolder.setRequestAttributes(requestAttributes);
   }
 }
