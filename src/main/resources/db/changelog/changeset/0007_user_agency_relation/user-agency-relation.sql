24ea6342f122a00056d242df5e27a1c2dffc2489
tkuzynow <tomasz.kuzynowski@gmail.com>
2022-10-28 11:59:50 +0200

    fix: Mysql changes incompatibilities

--- src/main/resources/db/changelog/changeset/0007_user_agency_relation/user-agency-relation.sql
+++ src/main/resources/db/changelog/changeset/0007_user_agency_relation/user-agency-relation.sql
@@ -2,8 +2,8 @@ CREATE TABLE `userservice`.`user_agency` (
   `id` bigint(21) unsigned NOT NULL,
   `user_id` varchar(36) COLLATE utf8_unicode_ci NOT NULL,
   `agency_id` bigint(21) unsigned NOT NULL,
-  `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
-  `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
+  `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
+  `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `chat_id` (`user_id`),
   CONSTRAINT `user_agency_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
--- src/main/resources/db/changelog/changeset/0030_session_counsellingrelation_and_topics/session-counselling-relation-and-topics.sql
+++ src/main/resources/db/changelog/changeset/0030_session_counsellingrelation_and_topics/session-counselling-relation-and-topics.sql
@@ -2,8 +2,8 @@ CREATE TABLE `userservice`.`session_topic` (
     `id` bigint(21) NOT NULL,
     `session_id` bigint(21) unsigned NOT NULL,
     `topic_id` bigint(21) unsigned NOT NULL,
-    `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
-    `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP(),
+    `create_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
+    `update_date` datetime NOT NULL DEFAULT UTC_TIMESTAMP,
     PRIMARY KEY (`id`),
     KEY `session_id` (`session_id`),
     CONSTRAINT `session_topic_ibfk_1` FOREIGN KEY (`session_id`) REFERENCES `userservice`.`session` (`Id`) ON UPDATE CASCADE
