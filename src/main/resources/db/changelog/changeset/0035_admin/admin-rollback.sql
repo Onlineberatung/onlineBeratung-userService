4d3b8856d920794ff5bfb309926aa25250c92e3d
Patric Dosch <patric.dosch@virtual-identity.com>
2022-11-18 16:22:21 +0100

    feat: IC-2021 Tenant admins can create agency admins AND counsellors
    - init db for Admin-Table

---
+++ src/main/resources/db/changelog/changeset/0035_admin/admin-rollback.sql
@@ -0,0 +1,6 @@
+DROP TRIGGER `userservice`.`admin_agency_update`;
+DROP SEQUENCE `userservice`.`sequence_admin_agency`;
+DROP TABLE `userservice`.`admin_agency`;
+
+DROP TRIGGER `userservice`.`admin_update`;
+DROP TABLE `userservice`.`admin`;
