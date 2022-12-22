200004f49a9cb8d120100bc506b7b6c6156dc69e
Torsten Krohn <torsten.krohn@webatspeed.com>
2022-12-22 13:57:35 +0100

    test: add Liquibase hibernate sync check

---
+++ src/test/java/de/caritas/cob/userservice/api/model/LiquibaseHibernateTest.java
@@ -0,0 +1,48 @@
+package de.caritas.cob.userservice.api.model;
+
+import java.util.UUID;
+import javax.persistence.EntityManagerFactory;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MariaDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@SpringBootTest(
+    properties = {
+      "spring.jpa.hibernate.ddl-auto=validate",
+      "identity.technical-user.username=tech-user",
+      "identity.technical-user.password=tech-pass"
+    })
+public class LiquibaseHibernateTest {
+  @Container
+  @SuppressWarnings("resource")
+  static MariaDBContainer<?> DB =
+      new MariaDBContainer<>("mariadb:10.5.10").withDatabaseName("userservice");
+
+  @Autowired EntityManagerFactory entityManagerFactory;
+
+  @DynamicPropertySource
+  static void registerMySQLProperties(DynamicPropertyRegistry registry) {
+    registry.add("spring.datasource.url", DB::getJdbcUrl);
+    registry.add("spring.datasource.username", DB::getUsername);
+    registry.add("spring.datasource.password", DB::getPassword);
+  }
+
+  @Test
+  void databaseAndEntityMappingsShouldBeInSync() {
+    var entityManager = entityManagerFactory.createEntityManager();
+    try {
+      entityManager.find(User.class, UUID.randomUUID().toString());
+    } finally {
+      entityManager.close();
+    }
+  }
+}
