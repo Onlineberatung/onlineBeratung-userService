bb97a8342235d35798b3089712ceec2599915154
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-03-08 14:09:55 +0100

    fix: add missing semicolon to script

--- src/main/resources/db/changelog/changeset/0040_add_notification_settings/add-notification-settings.sql
+++ src/main/resources/db/changelog/changeset/0040_add_notification_settings/add-notification-settings.sql
@@ -7,7 +7,7 @@ ALTER TABLE `userservice`.`consultant`
 UPDATE `userservice`.`consultant`
 SET notifications_settings = "{'initialEnquiryNotificationEnabled': 'true','newChatMessageNotificationEnabled': 'true', 'reassignmentNotificationEnabled': 'true','appointmentNotificationEnabled': 'true'}",
     notifications_enabled = 1
-WHERE email is not NULL and email <> ''
+WHERE email is not NULL and email <> '';
 
 ALTER TABLE `userservice`.`user`
     ADD COLUMN `notifications_enabled` tinyint(4) unsigned NOT NULL DEFAULT '0';
@@ -18,4 +18,4 @@ ALTER TABLE `userservice`.`user`
 UPDATE `userservice`.`user`
 SET notifications_settings = "{'initialEnquiryNotificationEnabled': 'true','newChatMessageNotificationEnabled': 'true', 'reassignmentNotificationEnabled': 'true','appointmentNotificationEnabled': 'true'}",
     notifications_enabled = 1
-WHERE email is not NULL and email <> ''
+WHERE email is not NULL and email <> '';
