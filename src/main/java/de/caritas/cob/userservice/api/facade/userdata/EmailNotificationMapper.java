3445aefd41ac1a14ee6e10c0e1a6faa1b7d47e32
tkuzynow <tomasz.kuzynowski@gmail.com>
2023-03-08 13:15:33 +0100

    fix: tests

--- src/main/java/de/caritas/cob/userservice/api/facade/userdata/EmailNotificationMapper.java
+++ src/main/java/de/caritas/cob/userservice/api/facade/userdata/EmailNotificationMapper.java
@@ -5,6 +5,7 @@ import static de.caritas.cob.userservice.api.helper.json.JsonDeserializer.deseri
 import de.caritas.cob.userservice.api.adapters.web.dto.EmailNotificationsDTO;
 import de.caritas.cob.userservice.api.adapters.web.dto.NotificationsSettingsDTO;
 import de.caritas.cob.userservice.api.model.NotificationsAware;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,7 +14,7 @@ public class EmailNotificationMapper {
   EmailNotificationsDTO toEmailNotificationsDTO(NotificationsAware notificationsAware) {
     EmailNotificationsDTO emailNotificationsDTO = new EmailNotificationsDTO();
     emailNotificationsDTO.emailNotificationsEnabled(notificationsAware.isNotificationEnabled());
-    if (notificationsAware.getNotificationSettings() == null) {
+    if (StringUtils.isBlank(notificationsAware.getNotificationSettings())) {
       emailNotificationsDTO.settings(new NotificationsSettingsDTO());
     } else {
       emailNotificationsDTO.settings(
